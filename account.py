c09b19b680a7427880a98825ee7e91ae468be2e2
Vicky kumar <vickyin841216@gmail.com>
2026-04-26 03:30:19 +0530

    Add account data structure with sample accounts

---
+++ account.py
@@ -0,0 +1,20 @@
+accounts = {
+    "1001": {
+        "name": "Vicky",
+        "pin": "9798",
+        "balance": 10000,
+        "transactions": []
+    },
+    "1002": {
+        "name": "Shobha",
+        "pin": "9142",
+        "balance": 15000,
+        "transactions": []
+    },
+    "1003": {
+        "name": "Rahul",
+        "pin": "8766",
+        "balance": 80000,
+        "transactions": []
+    }
+}
